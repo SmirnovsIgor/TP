c8587dfe9fadaf76332b7fd42a415997a31b9dcc
Ed <edgar-str@mail.ru>
2019-08-19 17:01:32 +0300

    IDModel was added

--- apps/base/models.py
+++ apps/base/models.py
@@ -1,9 +1,20 @@
+import uuid
+
 from django.db import models
 
 
 class CreateModifyModel(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class IDModel(models.Model):
+    id = models.UUIDField(primary_key=True,
+                          default=uuid.uuid4,
+                          editable=False)
 
     class Meta:
         abstract = True
