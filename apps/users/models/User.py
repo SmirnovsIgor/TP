460fc41fb3f7e9cf03fab768cdb6c5f73adfda94
Shota <shotaik@mail.ru>
2019-08-21 13:54:49 +0300

    Add email blank=False

--- apps/users/models/User.py
+++ apps/users/models/User.py
@@ -1,9 +1,10 @@
 from tools.image_funcs import get_image_path
 from django.contrib.auth.models import AbstractUser
-from django.db.models import ImageField
+from django.db import models
 
 from apps.base.models import BaseModel
 
 
 class User(AbstractUser, BaseModel):
-    profile_image = ImageField(upload_to=get_image_path, blank=True, null=True)
+    profile_image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+    email = models.EmailField('email address', blank=False)
