edb31108e4f9f185dbf5e89ea452e632171c5e18
Shota <shotaik@mail.ru>
2019-08-21 15:01:50 +0300

    Add some fields

--- apps/users/models/User.py
+++ apps/users/models/User.py
@@ -7,4 +7,7 @@ from apps.base.models import BaseModel
 
 class User(AbstractUser, BaseModel):
     profile_image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
-    email = models.EmailField('email address', blank=False)
+    email = models.EmailField('email address', blank=False, null=False)
+    first_name = models.CharField('first name', max_length=30, blank=True, null=True)
+    last_name = models.CharField('last name', max_length=150, blank=True, null=True)
+
