04463ff40bceb5244b399eaaa6be450e6898ebcb
Shota <shotaik@mail.ru>
2019-08-21 15:47:37 +0300

    Add FK to Organization

--- apps/users/models/User.py
+++ apps/users/models/User.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from apps.base.models import BaseModel
+from apps.users.models import Organization
 
 
 class User(AbstractUser, BaseModel):
@@ -10,4 +11,4 @@ class User(AbstractUser, BaseModel):
     email = models.EmailField('email address', blank=False, null=False)
     first_name = models.CharField('first name', max_length=30, blank=True, null=True)
     last_name = models.CharField('last name', max_length=150, blank=True, null=True)
-
+    organization = models.ForeignKey('Organization', on_delete=models.CASCADE, null=True)
