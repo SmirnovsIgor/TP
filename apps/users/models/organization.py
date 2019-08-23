20624e7e13fbe6b9a2d3155fd3a3cf0662279fab
igor_smirnov <SmirnovsIgor>
2019-08-23 13:30:27 +0300

    Update organization model

--- apps/users/models/organization.py
+++ apps/users/models/organization.py
@@ -6,5 +6,6 @@ from apps.base.models import BaseModel
 class Organization(BaseModel):
     name = models.CharField(max_length=150, blank=False, null=False)
     email = models.EmailField(blank=False, null=False)
-    description = models.TextField()
     profile_image = models.ImageField(upload_to=get_image_path, blank=True, null=True)
+    description = models.TextField(blank=True, null=False)
+    approved = models.BooleanField(default=False)
