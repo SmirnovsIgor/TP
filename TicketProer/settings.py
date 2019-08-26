79eb96c6c2ac4563f4dd60661a46f80605053d30
Ed <edgar-str@mail.ru>
2019-08-26 13:12:52 +0300

    resolve conflicts

--- TicketProer/settings.py
+++ TicketProer/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.base',
-    'apps.authorization',
+    'apps.users',
+    'apps',
     'apps.events',
 ]
 
--- tools/image_funcs.py
+++ tools/image_funcs.py
@@ -1,8 +1,15 @@
 import os
 
 
+def get_image_path(instance: object, filename: str):
+    """
+    Get path for /static/photos/{instance_id}/file/{file_name}
+    """
+    return os.path.join('photos', instance.id, filename)
+
+
 def get_posters_path(instance: object, filename: str):
     """
-    Gets path for event's poster '/static/posters/{event_id}/{filename}
+    Gets path for event's poster '/static/posters/{event_id}/file/{filename}
     """
     return os.path.join('posters', instance.id, filename)
