612b8bdc0fdf44ee7414524954a440d6cced9709
Ed <edgar-str@mail.ru>
2019-08-26 18:26:49 +0300

    save method was rewritten, migrations were added

--- apps/events/models/event.py
+++ apps/events/models/event.py
@@ -9,24 +9,6 @@ from tools.image_funcs import get_posters_path
 
 
 class Event(BaseAbstractModel):
-    """
-    Event Model inherited from BaseModel.
-    Fields id, created, updated were inherited
-    """
-    name = models.CharField(max_length=64)
-    description = models.TextField()
-    poster = models.ImageField(upload_to=get_posters_path, blank=True, null=True)
-    organizer_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    organizer_id = models.UUIDField()
-    organizer = GenericForeignKey('organizer_type', 'organizer_id')
-    place = models.ForeignKey(Place, on_delete=models.CASCADE, null=True, blank=True)
-    address = models.ForeignKey(Address, on_delete=models.CASCADE, null=False, blank=False)
-    date = models.DateTimeField()
-    duration = models.DurationField(default=False)
-    age_rate = models.PositiveSmallIntegerField()
-    is_approved = models.BooleanField()
-    max_members = models.PositiveIntegerField()
-    ratings = models.DecimalField(max_digits=2, decimal_places=1)
     SOON = "SOON"
     SUCCEED = "SUCCEED"
     REJECTED = "REJECTED"
@@ -35,12 +17,30 @@ class Event(BaseAbstractModel):
         (SUCCEED, "succeed"),
         (REJECTED, 'rejected'),
     )
+    name = models.CharField(max_length=64, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
+    poster = models.ImageField(upload_to=get_posters_path, blank=True, null=True)
+    organizer_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    organizer_id = models.UUIDField()
+    organizer = GenericForeignKey('organizer_type', 'organizer_id')
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, blank=True, null=True)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE, blank=False, null=False)
+    date = models.DateTimeField(blank=False, null=False)
+    duration = models.DurationField(blank=False, null=False)
+    age_rate = models.PositiveSmallIntegerField(default=18, blank=False, null=False)
+    is_approved = models.BooleanField(default=False)
+    max_members = models.PositiveIntegerField(blank=False, null=False)
     status = models.CharField(max_length=16, choices=STATUS_TYPES, default=SOON)
 
     def __str__(self):
         return self.name
 
     def save(self, *args, **kwargs):
-        if not Event.objects.get(organizer_id=self.organizer_id).exists():
+        if self._state.adding:
             self.organizer = self.organizer.membership.organization if self.organizer.membership else self.organizer
         super().save(self, *args, **kwargs)
+
+    # TODO
+    # @property
+    # def rating(self):
+    #     pass
