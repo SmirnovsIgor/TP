126289ee67c5bb7f70c4e5bd534377bdf16ee53f
Shota <shotaik@mail.ru>
2019-08-28 16:52:18 +0300

    Add user serializer

---
+++ apps/users/serializators/user_serializer.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.UUIDField()
+    profile_image = serializers.ImageField(required=False)
+    email = serializers.EmailField(required=False)
+    first_name = serializers.CharField(required=False, max_length=30)
+    second_name = serializers.CharField(required=False, max_length=150)
+    username = serializers.CharField(required=True, max_lengh=150)
+    is_staff = serializers.BooleanField(default=False)
+    is_active = serializers.BooleanField(default=True)
