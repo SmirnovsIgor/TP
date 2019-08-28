901184e6486529f62d30687799473a1f701861d2
Shota <shotaik@mail.ru>
2019-08-28 16:54:52 +0300

    Rename field

--- apps/users/serializators/user_serializer.py
+++ apps/users/serializators/user_serializer.py
@@ -6,7 +6,7 @@ class UserSerializer(serializers.Serializer):
     profile_image = serializers.ImageField(required=False)
     email = serializers.EmailField(required=False)
     first_name = serializers.CharField(required=False, max_length=30)
-    second_name = serializers.CharField(required=False, max_length=150)
+    last_name = serializers.CharField(required=False, max_length=150)
     username = serializers.CharField(required=True, max_lengh=150)
     is_staff = serializers.BooleanField(default=False)
     is_active = serializers.BooleanField(default=True)
