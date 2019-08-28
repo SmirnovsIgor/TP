3fcfd7bf2e49a16f064440f9051b1a531e5326e1
Shota <shotaik@mail.ru>
2019-08-28 16:59:15 +0300

    Add more validators

--- apps/users/serializators/user_serializer.py
+++ apps/users/serializators/user_serializer.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 
 
 class UserSerializer(serializers.Serializer):
-    id = serializers.UUIDField()
-    profile_image = serializers.ImageField(required=False)
-    email = serializers.EmailField(required=False)
-    first_name = serializers.CharField(required=False, max_length=30)
-    last_name = serializers.CharField(required=False, max_length=150)
-    username = serializers.CharField(required=True, max_lengh=150)
-    is_staff = serializers.BooleanField(default=False)
-    is_active = serializers.BooleanField(default=True)
+    id = serializers.UUIDField(read_only=True)
+    profile_image = serializers.ImageField(required=False, allow_empty_file=True, allow_null=True)
+    email = serializers.EmailField(required=True, allow_blank=False, allow_null=False)
+    first_name = serializers.CharField(required=False, allow_null=True, allow_blank=True, max_length=30)
+    last_name = serializers.CharField(required=False, allow_null=True, allow_blank=True, max_length=150)
+    username = serializers.CharField(required=True, allow_null=False, allow_blank=False, max_lengh=150)
+    is_staff = serializers.BooleanField(default=False, allow_null=False)
+    is_active = serializers.BooleanField(default=True, allow_null=False)
