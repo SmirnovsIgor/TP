7db0829a3062985d06c576aeb560b670d18beba3
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-08-30 13:25:14 +0300

    Fixed attributes typos.

--- apps/locations/serializers/place_serializer.py
+++ apps/locations/serializers/place_serializer.py
@@ -9,5 +9,5 @@ class PlaceSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=75, allow_blank=False, allow_null=False)
     address = AddressSerializer()
     photo = serializers.ImageField(required=False, allow_empty_file=True, allow_null=True)
-    description = serializers.CharField(max_length=200, allow_blank=False, allow_null=False)
+    description = serializers.CharField(max_length=200, allow_blank=True, allow_null=True)
     status = serializers.ChoiceField(choices=Place.STATUS_CHOICES)
