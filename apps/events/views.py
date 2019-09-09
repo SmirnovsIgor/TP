f8342bd0bebb474462a714e5dcaf5848ddef633b
leucopterus <edgar-str@mail.ru>
2019-09-09 21:17:53 +0300

    event endpoint is ready for filtering and ordering

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,4 +1,5 @@
 from django_filters import rest_framework as filters
+from rest_framework import filters as rest_filters
 from rest_framework import viewsets
 
 from apps.events.models import Event
@@ -25,6 +26,7 @@ class EventViewSet(viewsets.ModelViewSet):
     """
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    filter_backends = [filters.OrderingFilter, filters.DjangoFilterBackend]
+    filter_backends = [filters.DjangoFilterBackend, rest_filters.OrderingFilter]
     filter_class = EventFilter
+    ordering_fields = ("created",)
     ordering = ("created",)
