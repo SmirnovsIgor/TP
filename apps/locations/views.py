27a95865fe418aeb00f21cb7aeff74fcfed3bde1
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-09-10 09:12:24 +0300

    implemented Place REST API

--- apps/locations/views.py
+++ apps/locations/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from apps.locations.models import Place
+from apps.locations.serializers import PlaceSerializer
+
+
+class PlaceViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for listing Places
+    """
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
