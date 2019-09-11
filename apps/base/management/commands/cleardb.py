996d3adf2de5f11379905ca08da863a4d2c6782a
Ed <edgar-str@mail.ru>
2019-09-11 11:05:15 +0300

    deleted CommandError from cleardb file

--- apps/base/management/commands/cleardb.py
+++ apps/base/management/commands/cleardb.py
@@ -1,4 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from apps.users.models import User, Organization
 from apps.locations.models import Address, Place
