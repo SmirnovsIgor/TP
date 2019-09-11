74e66209353b7884c1729dd7c1368b9cc14ef230
Ed <edgar-str@mail.ru>
2019-09-11 15:31:02 +0300

    clear event view

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,19 +1,7 @@
-from django_filters import rest_framework as filters
 from rest_framework import viewsets
 
 from apps.events.models import Event
 from apps.events.serializers import EventSerializer
-
-
-class EventFilter(filters.FilterSet):
-
-    class Meta:
-        model = Event
-        fields = {
-            "date": ["gte", "lte"],
-            "is_hot": ["exact"],
-            "is_top": ["exact"],
-        }
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -22,5 +10,3 @@ class EventViewSet(viewsets.ModelViewSet):
     """
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    filter_backends = [filters.DjangoFilterBackend]
-    filterset_class = EventFilter
