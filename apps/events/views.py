7208f93737021011666335651701d457dac2238a
Ed <edgar-str@mail.ru>
2019-09-11 12:57:28 +0300

    filtering were deleted

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,23 +1,7 @@
-from django_filters import rest_framework as filters
-from rest_framework import filters as rest_filters
 from rest_framework import viewsets
 
 from apps.events.models import Event
 from apps.events.serializers import EventSerializer
-
-
-class EventFilter(filters.FilterSet):
-    place = filters.UUIDFilter(field_name="place_id")
-    address = filters.UUIDFilter(field_name="address_id")
-    organizer = filters.UUIDFilter(field_name="organizer_id")
-    date__gte = filters.DateTimeFilter(field_name="date", lookup_expr="gte")
-    date__lte = filters.DateTimeFilter(field_name="date", lookup_expr="lte")
-    is_top = filters.BooleanFilter(field_name="is_top")
-    is_hot = filters.BooleanFilter(field_name="is_hot")
-
-    class Meta:
-        model = Event
-        fields = ["place", "address", "organizer", "date__lte", "date__gte", "is_top", "is_hot"]
 
 
 class EventViewSet(viewsets.ModelViewSet):
@@ -26,7 +10,3 @@ class EventViewSet(viewsets.ModelViewSet):
     """
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-    filter_backends = [filters.DjangoFilterBackend, rest_filters.OrderingFilter]
-    filter_class = EventFilter
-    ordering_fields = ("created",)
-    ordering = ("created",)
