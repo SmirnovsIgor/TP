64ca6f20dfd62f72e7525b007794128445f94dee
Ed <edgar-str@mail.ru>
2019-09-12 12:22:29 +0300

    minor fixes in populatedb

--- apps/base/management/commands/populatedb.py
+++ apps/base/management/commands/populatedb.py
@@ -18,13 +18,13 @@ class Command(BaseCommand):
     help = 'Populate DB'
 
     def add_arguments(self, parser):
-        parser.add_argument('great_count', nargs='?', type=int, help='maximum is 100 objects')
+        parser.add_argument('great_count', nargs='?', type=int, help='maximum is 100 objects for ')
         parser.add_argument('less_count', nargs='?', type=int, help='maximum is 80 objects')
 
     def handle(self, *args, **options):
         big = DEFAULT_BIG_NUMBER if not options['great_count'] else options['great_count']
         small = DEFAULT_SMALL_NUMBER if not options['less_count'] else options['less_count']
-        event_user_addr_place = small//2
+        event_user_addr_place = (big-small)//2
         event_user_addr = (big-small) - event_user_addr_place
         event_org_addr_place = small//2
         event_org_addr = small - event_org_addr_place
