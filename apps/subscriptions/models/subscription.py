a2d4e22d1f8c1dd274b9eb661a7e41418a58c4cb
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-09-13 11:08:37 +0300

    Added forgotten field.

--- apps/subscriptions/models/subscription.py
+++ apps/subscriptions/models/subscription.py
@@ -6,16 +6,19 @@ from apps.users.models import User
 
 
 class Subscription(BaseAbstractModel):
-    class Meta:
-        unique_together = ['user_id', 'event_id']
-
     STATUS_ACTIVE = 'ACTIVE'
     STATUS_REJECTED = 'REJECTED'
     STATUS_UNPAID = 'UNPAID'
+    STATUS_UNTRACKED = 'UNTRACKED'
     STATUS_CHOICES = [
         (STATUS_ACTIVE, 'Active'),
         (STATUS_REJECTED, 'Rejected'),
         (STATUS_UNPAID, 'Unpaid'),
+        (STATUS_UNTRACKED, 'Untracked')
     ]
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='events')
     event_id = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='subscribers')
+    status = models.CharField(max_length=9, choices=STATUS_CHOICES, blank=False, null=False, default=STATUS_UNTRACKED)
+
+    class Meta:
+        unique_together = ['user_id', 'event_id']
