8289b3ced2310cf68d4eb6ca33dcbeca98e0151d
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-09-16 16:57:53 +0300

    Improved tests. Fixed factory.

--- apps/locations/tests/test_place.py
+++ apps/locations/tests/test_place.py
@@ -1,5 +1,6 @@
 import pytest
 import faker
+import uuid
 
 
 @pytest.mark.django_db
@@ -8,10 +9,22 @@ class TestPlaces:
         res = client.get(f'/api/places/{place.id}/')
         assert res.status_code == 200
         place_dict = res.json()
-        assert place_dict.get('name')
-        assert place_dict.get('address')
-        assert place_dict.get('description')
-        assert place_dict.get('status')
+        assert uuid.UUID(place_dict.get('id')) == place.id
+        assert place_dict.get('name') == place.name
+
+        address = place_dict.get('address')
+        assert address
+        assert uuid.UUID(address.get('id')) == place.address.id
+        assert address.get('country') == place.address.country
+        assert address.get('city') == place.address.city
+        assert address.get('street') == place.address.street
+        assert address.get('house') == place.address.house
+        assert address.get('floor') == place.address.floor
+        assert address.get('apartments') == place.address.apartments
+        assert address.get('description') == place.address.description
+
+        assert place_dict.get('description') == place.description
+        assert place_dict.get('status') == place.status
 
     @pytest.mark.parametrize('place_qty', [0, 1, 10, 100])
     def test_list(self, client, places, place_qty):
