ebfd566be7cf6708899db7c9d506e88df9a18a38
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-09-17 11:33:42 +0300

    Fixed status checks. Fixed id checks.

--- apps/locations/tests/test_place.py
+++ apps/locations/tests/test_place.py
@@ -1,20 +1,21 @@
 import pytest
 import faker
-import uuid
+
+from rest_framework import status
 
 
 @pytest.mark.django_db
 class TestPlaces:
     def test_detail(self, client, place):
         res = client.get(f'/api/places/{place.id}/')
-        assert res.status_code == 200
+        assert res.status_code == status.HTTP_200_OK
         place_dict = res.json()
-        assert uuid.UUID(place_dict.get('id')) == place.id
+        assert place_dict.get('id') == str(place.id)
         assert place_dict.get('name') == place.name
 
         address = place_dict.get('address')
         assert address
-        assert uuid.UUID(address.get('id')) == place.address.id
+        assert address.get('id') == str(place.address.id)
         assert address.get('country') == place.address.country
         assert address.get('city') == place.address.city
         assert address.get('street') == place.address.street
@@ -29,10 +30,10 @@ class TestPlaces:
     @pytest.mark.parametrize('place_qty', [0, 1, 10, 100])
     def test_list(self, client, places, place_qty):
         res = client.get('/api/places/')
-        assert res.status_code == 200
+        assert res.status_code == status.HTTP_200_OK
         assert isinstance(res.json(), list)
         assert len(res.json()) == place_qty
 
     def test_detail_error(self, client):
         res = client.get(f'api/places/{faker.Faker().random_number()}/')
-        assert res.status_code == 404
+        assert res.status_code == status.HTTP_404_NOT_FOUND
