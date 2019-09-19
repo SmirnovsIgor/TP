758af90b1d77fa32570249e1e5d77364b92fd8f9
Ed <edgar-str@mail.ru>
2019-09-19 19:01:34 +0300

    raw create

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,8 +1,12 @@
+import uuid
+
+from django.contrib.contenttypes.models import ContentType
 from django_filters import rest_framework as filters
-from rest_framework import viewsets, mixins, status, filters as rest_filters
+from rest_framework import viewsets, mixins, status, exceptions, filters as rest_filters
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from apps.users.models import User, Organization, MembersList
 from apps.locations.models import Place, Address
 from apps.locations.serializers import ShortPlaceSerializer, AddressSerializer
 
@@ -42,8 +46,81 @@ class EventViewSet(mixins.CreateModelMixin,
     ordering = ("created",)
 
     def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        user = request.user
+        data_dict = request.data
+        place = None
+        if 'place' in data_dict:
+            place = data_dict.pop('place')
+            if place is None:
+                pass
+            elif place.get('id'):
+                place = self.get_created_object(place.get('id'), Place)
+                address = self.get_created_object(place.address__id, Address)
+                place_serializer = ShortPlaceSerializer(data=place)
+                place_serializer.is_valid(raise_exception=True)
+                address_serializer = AddressSerializer(data=address)
+                address_serializer.is_valid(raise_exception=True)
+                # data_dict['place'] = place_serializer
+                # data_dict['address'] = address_serializer
+            else:
+                raise exceptions.ParseError('Wrong place data: send place id or nothing')
+        if 'address' in data_dict and place is None:
+            address = data_dict.pop('address')
+            if address.get('id'):
+                address = self.get_created_object(address.get('id'), Address)
+                # data_dict['address'] = address
+            elif address:
+                # address creation
+                address_serializer = AddressSerializer(data=address)
+                if address_serializer.is_valid():
+                    address = Address.objects.create(**address_serializer.validated_data)
+                else:
+                    raise exceptions.ParseError('Address invalid')
+            else:
+                raise exceptions.ParseError('Address field is filled out improperly')
+        else:
+            raise exceptions.ParseError('Please, transmit address or place data')
+        # -----------check if user has a membership in organization --------------
+        try:
+            organizer = user.membership
+            organizer_id = organizer.id
+            organizer_type = ContentType.objects.get_for_model(Organization)
+        except User.membership.RelatedObjectDoesNotExist:
+            organizer = user
+            organizer_id = user.id
+            organizer_type = ContentType.objects.get_for_model(User)
+        finally:
+            data_dict['organizer_id'] = organizer_id
+            data_dict['organizer_type'] = organizer_type
+            data_dict['organizer_type_id'] = organizer_type.id
+        # ------------------------------------------------------------------------
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(data=data_dict)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
+        data_dict['address'] = address
+        data_dict['place'] = place
+        data_dict['organizer'] = organizer
+        data_dict
+        Event.objects.create(**data_dict)
+        return Response(status=status.HTTP_201_CREATED)
+        # event = self.perform_create(serializer, address=address, place=place)
+        # event_data = serializer_class(event).data
+        # headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def get_created_object(self, obj_id, cls):
+        try:
+            return cls.objects.get(id=uuid.UUID(obj_id))
+        except ValueError:
+            raise exceptions.ParseError('')
+        except cls.DoesNotExist:
+            raise exceptions.NotFound('')
+
+    def perform_create(self, serializer, **kwargs):
+        serializer.save(**kwargs)
+
+
+"""
+from apps.events.views import User, Event
+User.objects.filter(membership__isnull=True)
+"""
