9c0dbdc0936dfb39a222b9d4a2e6a81bb3446312
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-09-20 15:23:43 +0300

    Implemented endpoint.

--- apps/subscriptions/serializers/subscription_serializer.py
+++ apps/subscriptions/serializers/subscription_serializer.py
@@ -6,4 +6,4 @@ from apps.subscriptions.models import Subscription
 class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subscription
-        fields = ['__all__']
+        fields = '__all__'
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,9 +2,12 @@ from rest_framework import viewsets
 from rest_framework.mixins import RetrieveModelMixin
 from rest_framework.permissions import IsAdminUser
 from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from apps.users.models import User
 from apps.users.serializers import UserSerializer
+from apps.subscriptions.models import Subscription
+from apps.subscriptions.serializers import SubscriptionSerializer
 from tools.action_based_permission import ActionBasedPermission
 
 
@@ -14,5 +17,11 @@ class UserDataForStaffViewSet(RetrieveModelMixin,
     serializer_class = UserSerializer
     permission_classes = (ActionBasedPermission,)
     action_permissions = {
-        IsAdminUser: ['retrieve']
+        IsAdminUser: ['retrieve', 'subscriptions']
     }
+
+    @action(detail=True, methods=['get'])
+    def subscriptions(self, request, pk=None):
+        user = self.get_object()
+        serializer = SubscriptionSerializer(user.events, many=True)
+        return Response(serializer.data)
