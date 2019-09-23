5b6be1c8aa85c56cee3e9064b6a27389938ba370
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-09-23 14:58:54 +0300

    Implemented new factory for subscriptions for tests.

--- apps/subscriptions/factories.py
+++ apps/subscriptions/factories.py
@@ -5,6 +5,8 @@ from faker import Factory as FakeFactory
 from apps.users.models import User
 from apps.events.models import Event
 from apps.subscriptions.models import Subscription
+from apps.events.factories import EventUserWithoutPlaceFactory
+from apps.users.factories import UserFactory
 
 
 faker = FakeFactory.create()
@@ -13,6 +15,15 @@ faker = FakeFactory.create()
 class SubscriptionFactory(factory.django.DjangoModelFactory):
     user = factory.Iterator(User.objects.all())
     event = factory.Iterator(Event.objects.all())
+    status = factory.fuzzy.FuzzyChoice(Subscription.STATUS_CHOICES, getter=lambda c: c[0])
+
+    class Meta:
+        model = Subscription
+
+
+class SubscriptionFactoryForTests(factory.django.DjangoModelFactory):
+    user = factory.SubFactory(UserFactory)
+    event = factory.SubFactory(EventUserWithoutPlaceFactory)
     status = factory.fuzzy.FuzzyChoice(Subscription.STATUS_CHOICES, getter=lambda c: c[0])
 
     class Meta:
