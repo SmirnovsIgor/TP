929420e1746aa1003ec2a175d7487c5e1c219d44
igor_smirnov <SmirnovsIgor>
2019-09-23 15:29:57 +0300

    Fix permissions

--- TicketProer/settings.py
+++ TicketProer/settings.py
@@ -59,7 +59,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-        'rest_framework.permissions.IsAuthenticated'],
+        ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication'],
     'DEFAULT_FILTER_BACKENDS': [
