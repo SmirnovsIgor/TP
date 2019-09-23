ce8b086729ac245ffa3b280b81f9bbfd45f68f70
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-09-23 15:39:58 +0300

    Intermediate commit.

--- apps/subscriptions/views.py
+++ apps/subscriptions/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from apps.subscriptions.models import Subscription
+from apps.subscriptions.serializers import SubscriptionSerializer
+from tools.action_based_permission import ActionBasedPermission
+
+
+class SubscriptionViewSet(viewsets.ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+    permission_classes = (ActionBasedPermission,)
+    action_permissions = {
+        IsAdminUser: ['list'],
+    }
