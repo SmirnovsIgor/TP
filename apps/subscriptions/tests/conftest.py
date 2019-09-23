1186043a5e974c321085900c3d58ae76760d9315
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-09-23 14:59:18 +0300

    Implemented tests.

---
+++ apps/subscriptions/tests/conftest.py
@@ -0,0 +1,42 @@
+import pytest
+from pytest_factoryboy import register
+from rest_auth.models import TokenModel
+
+from apps.events.factories import (
+    EventUserWithoutPlaceFactory,
+    EventUserWithPlaceFactory,
+    EventOrganizerWithoutPlaceFactory,
+    EventOrganizerWithPlaceFactory
+)
+from apps.locations.factories import AddressFactory, PlaceFactory
+from apps.users.factories import UserFactory, OrganizationFactory
+from apps.subscriptions.factories import SubscriptionFactory, SubscriptionFactoryForTests
+
+
+token_model = TokenModel
+register(UserFactory, 'user')
+register(UserFactory, 'request_user')
+register(PlaceFactory, 'place')
+register(AddressFactory, 'address')
+register(OrganizationFactory, 'organization')
+register(EventUserWithoutPlaceFactory, 'event_created_by_user_without_place')
+register(EventUserWithPlaceFactory, 'event_created_by_user_with_place')
+register(EventOrganizerWithoutPlaceFactory, 'event_created_by_organization_without_place')
+register(EventOrganizerWithPlaceFactory, 'event_created_by_organization_with_place')
+register(SubscriptionFactory, 'subscription')
+
+
+@pytest.fixture
+def token(request_user):
+    token, _ = token_model.objects.get_or_create(user=request_user)
+    return token
+
+
+@pytest.fixture
+def subscription_qty():
+    return 1
+
+
+@pytest.fixture
+def subscriptions(subscription_qty):
+    return SubscriptionFactoryForTests.create_batch(size=subscription_qty)
---
+++ apps/subscriptions/tests/test_subscription.py
@@ -0,0 +1,21 @@
+import pytest
+from rest_framework import status
+
+
+@pytest.mark.django_db
+class TestStaff:
+    @pytest.mark.parametrize('subscription_qty', [0, 1, 10, 50])
+    def test_user_subscriptions_for_staff_true(self, client, request_user, user, token, subscriptions, subscription_qty):
+        request_user.is_staff = True
+        request_user.save()
+        duplicates = 0
+        for subscription in subscriptions:
+            subscription.user = user
+            subscription.save()
+        res = client.get(f'/api/users/{user.id}/subscriptions/', **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
+        assert res.status_code == status.HTTP_200_OK
+        assert len(res.json()) == subscription_qty - duplicates
+
+    def test_user_subscriptions_for_staff_false(self, client, request_user, user, token):
+        response = client.get(f'/api/users/{user.id}/subscriptions/', **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
