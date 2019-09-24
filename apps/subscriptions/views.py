c462b24b01a2c904af7fe70d61ab3f04fa897584
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-09-24 17:48:00 +0300

    Intermediate commit.

--- apps/subscriptions/views.py
+++ apps/subscriptions/views.py
@@ -1,5 +1,8 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAdminUser
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
+from tools.custom_permissions import IsSubscriberOrAdmin
 
 from apps.subscriptions.models import Subscription
 from apps.subscriptions.serializers import SubscriptionSerializer
@@ -11,5 +14,14 @@ class SubscriptionViewSet(viewsets.ModelViewSet):
     serializer_class = SubscriptionSerializer
     permission_classes = (ActionBasedPermission,)
     action_permissions = {
-        IsAdminUser: ['list'],
+        IsAdminUser: ['list', 'update', 'partial_update'],
+        IsAuthenticated: ['create', 'destroy'],
+        IsSubscriberOrAdmin: ['retrieve'],
     }
+
+    @action(detail=True, methods=['post'])
+    def approve(self, request, pk=None):
+        subscription = self.get_object()
+        subscription.approve()
+        serializer = SubscriptionSerializer(subscription)
+        return Response(serializer.data)
