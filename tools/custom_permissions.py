85355b6b50e1b074eef00e31d5c9ed2fbc60b7cf
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-09-24 17:47:29 +0300

    Added custom permission.

--- tools/custom_permissions.py
+++ tools/custom_permissions.py
@@ -4,3 +4,8 @@ from rest_framework.permissions import BasePermission
 class IsOwnerOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
         return bool(request.user.is_staff or request.user.id == obj.created_by.id)
+
+
+class IsSubscriberOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return bool(request.user.is_staff or request.user.id == obj.user.id)
