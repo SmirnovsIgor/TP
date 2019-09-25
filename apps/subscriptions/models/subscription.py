4f954e4bbce9b01861bd4bc8beb761a7fa3a267c
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-09-25 16:02:55 +0300

    Fixed SubscriptionModel.

--- apps/subscriptions/models/subscription.py
+++ apps/subscriptions/models/subscription.py
@@ -10,11 +10,13 @@ class Subscription(BaseAbstractModel):
     STATUS_REJECTED = 'REJECTED'
     STATUS_UNPAID = 'UNPAID'
     STATUS_UNTRACKED = 'UNTRACKED'
+    STATUS_CANCELLED = 'CANCELLED'
     STATUS_CHOICES = [
         (STATUS_ACTIVE, 'Active'),
         (STATUS_REJECTED, 'Rejected'),
         (STATUS_UNPAID, 'Unpaid'),
-        (STATUS_UNTRACKED, 'Untracked')
+        (STATUS_UNTRACKED, 'Untracked'),
+        (STATUS_CANCELLED, 'Cancelled'),
     ]
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscriptions')
     event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='subscribers')
@@ -22,6 +24,10 @@ class Subscription(BaseAbstractModel):
 
     class Meta:
         unique_together = ['user', 'event']
+
+    def set_status(self, status):
+        self.status = status
+        self.save()
 
     def __str__(self):
         return f'{self.event.name}: {self.user.email}'
