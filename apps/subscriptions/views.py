9e65b34e11ac339cbbd0f8467d68bce6b0d56ec6
IgorSmirnov <46046688+SmirnovsIgor@users.noreply.github.com>
2019-09-26 12:55:16 +0300

    Implement users/me/subscriptions endpoint with filtering and ordering (#44)
    * Implement users/me/subscriptions endpoint with filtering and ordering
    
    * Fix views and urls

--- apps/subscriptions/views.py
+++ apps/subscriptions/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import filters as rest_filters, viewsets
+from django_filters import rest_framework as filters
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+from apps.subscriptions.models import Subscription
+from apps.subscriptions.serializers import SubscriptionSerializer
+
+
+class SubscriptionFilter(filters.FilterSet):
+    event__date__gte = filters.DateTimeFilter(field_name='event__date', lookup_expr="gte")
+    event__date__lte = filters.DateTimeFilter(field_name='event__date', lookup_expr="lte")
+
+    class Meta:
+        model = Subscription
+        fields = ['user', 'event', 'status', 'event__date__lte', 'event__date__gte']
+
+
+class SubscriptionViewSet(viewsets.ReadOnlyModelViewSet):
+
+    model = Subscription
+    serializer_class = SubscriptionSerializer
+    filter_backends = [filters.DjangoFilterBackend, rest_filters.OrderingFilter]
+    filterset_class = SubscriptionFilter
+
+    ordering_fields = ('event__date',)
+    ordering = ('event__date',)
+
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return self.request.user.all_active_subscriptions
