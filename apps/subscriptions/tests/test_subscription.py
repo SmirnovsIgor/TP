f23be22130ad14e2d829322fb8fef988943a7de1
Mikhail Tupitsyn <praid.mikhail@yandex.by>
2019-09-27 16:59:06 +0300

    Implemented tests for post for subscriptions.

--- apps/subscriptions/tests/test_subscription.py
+++ apps/subscriptions/tests/test_subscription.py
@@ -1,7 +1,10 @@
-import pytest
+import datetime
 import json
+
+import pytest
 from rest_framework import status
 
+from apps.events.models import Event
 from apps.subscriptions.models import Subscription
 
 
@@ -114,29 +117,131 @@ class TestSubscriptions:
         res = client.delete(f'/api/subscriptions/{subscription.id}/', **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
         assert res.status_code == status.HTTP_404_NOT_FOUND
 
-    def test_delete_detail_for_random_id(self, client, request_user, token, subscription):
+    def test_destroy_detail_for_random_id(self, client, request_user, token, subscription):
         subscription.user = request_user
         subscription.status = Subscription.STATUS_CANCELLED
         subscription.save()
         res = client.delete(f'/api/subscriptions/ghjklasfg/', **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
         assert res.status_code == status.HTTP_404_NOT_FOUND
 
-    def test_patch_for_staff(self, client, request_user, token, subscription):
+    def test_partial_update_for_staff(self, client, request_user, token, subscription):
         request_user.is_staff = True
         request_user.save()
         subscription.status = Subscription.STATUS_ACTIVE
         subscription.save()
         new_data = {'status': 'REJECTED'}
-        res = client.patch(f'/api/subscriptions/{subscription.id}/', data=json.dumps(new_data),
-                           content_type='application/json', **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
+        res = client.patch(f'/api/subscriptions/{subscription.id}/',
+                           data=json.dumps(new_data),
+                           content_type='application/json',
+                           **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
         assert res.status_code == status.HTTP_200_OK
         data = res.json()
         assert data.get('status') == new_data.get('status')
 
-    def test_patch_for_regular_user(self, client, request_user, token, subscription):
+    def test_partial_update_for_regular_user(self, client, request_user, token, subscription):
         subscription.status = Subscription.STATUS_ACTIVE
         subscription.save()
         new_data = {'status': 'REJECTED'}
-        res = client.patch(f'/api/subscriptions/{subscription.id}/', data=json.dumps(new_data),
-                           content_type='application/json', **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
+        res = client.patch(f'/api/subscriptions/{subscription.id}/',
+                           data=json.dumps(new_data),
+                           content_type='application/json',
+                           **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
         assert res.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_create_ok(self, client, request_user, event, token):
+        sub = {'event': f'{str(event.id)}'}
+        event.status = Event.SOON
+        event.date += datetime.timedelta(50)
+        event.save()
+        res = client.post(f'/api/subscriptions/',
+                          data=json.dumps(sub),
+                          content_type='application/json',
+                          **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
+        assert res.status_code == status.HTTP_201_CREATED
+        data = res.json()
+        assert data.get('user') == str(request_user.id)
+        assert data.get('event') == str(event.id)
+        assert data.get('status') == Subscription.STATUS_UNTRACKED
+
+    def test_create_not_authenticated(self, client, event):
+        sub = {'event': f'{str(event.id)}'}
+        res = client.post(f'/api/subscriptions/', data=json.dumps(sub), content_type='application/json')
+        assert res.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_create_without_event_id(self, client, request_user, token):
+        sub = {}
+        res = client.post(f'/api/subscriptions/',
+                          data=json.dumps(sub),
+                          content_type='application/json',
+                          **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
+        assert res.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_create_with_empty_event_id(self, client, request_user, token):
+        sub = {'event': ''}
+        res = client.post(f'/api/subscriptions/',
+                          data=json.dumps(sub),
+                          content_type='application/json',
+                          **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
+        assert res.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_create_with_bad_event_id(self, client, request_user, token):
+        sub = {'event': 'jhggkjh'}
+        res = client.post(f'/api/subscriptions/',
+                          data=json.dumps(sub),
+                          content_type='application/json',
+                          **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
+        assert res.status_code == status.HTTP_400_BAD_REQUEST
+
+    def test_create_with_wrong_event_id(self, client, request_user, token):
+        sub = {'event': 'cee51c29-1c13-493e-bbdd-f2fbf931dd35'}
+        res = client.post(f'/api/subscriptions/',
+                          data=json.dumps(sub),
+                          content_type='application/json',
+                          **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
+        assert res.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_create_with_unsubscribabale_event(self, client, request_user, event, token):
+        sub = {'event': f'{str(event.id)}'}
+        event.status = Event.SUCCEED
+        event.date -= datetime.timedelta(50)
+        event.save()
+        res = client.post(f'/api/subscriptions/',
+                          data=json.dumps(sub),
+                          content_type='application/json',
+                          **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
+        assert res.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_create_with_existing_subscription(self, client, request_user, event, token, subscription):
+        sub = {'event': f'{str(event.id)}'}
+        event.status = Event.SOON
+        event.date += datetime.timedelta(50)
+        event.save()
+        subscription.user = request_user
+        subscription.event = event
+        subscription.status = Subscription.STATUS_ACTIVE
+        subscription.save()
+        res = client.post(f'/api/subscriptions/',
+                          data=json.dumps(sub),
+                          content_type='application/json',
+                          **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
+        assert res.status_code == status.HTTP_409_CONFLICT
+
+    def test_create_with_deleted_subscription(self, client, request_user, event, token, subscription):
+        sub = {'event': f'{str(event.id)}'}
+        event.status = Event.SOON
+        event.date += datetime.timedelta(50)
+        event.save()
+        subscription.user = request_user
+        subscription.event = event
+        subscription.status = Subscription.STATUS_CANCELLED
+        subscription.save()
+        res = client.post(f'/api/subscriptions/',
+                          data=json.dumps(sub),
+                          content_type='application/json',
+                          **{'HTTP_AUTHORIZATION': f'Token {str(token)}'})
+        assert res.status_code == status.HTTP_201_CREATED
+        data = res.json()
+        assert data.get('user') == str(request_user.id)
+        assert data.get('event') == str(event.id)
+        assert data.get('status') == Subscription.STATUS_UNTRACKED
+
